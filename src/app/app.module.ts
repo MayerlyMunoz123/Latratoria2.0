d1167d6c4d397af7a8ea6108d5ac11fc1bdffe46
MayerlyMunoz123 <mml.munoz@yavirac.edu.ec>
2023-11-28 22:54:17 -0500

    Funcional

---
+++ src/app/app.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { AmbientesComponent } from './ambientes/ambientes/ambientes/ambientes.component';
+import { RouterModule } from '@angular/router';
+import { FormularioComponent } from './auth/formulario/formulario.component';
+import { Ambiente1Module} from './ambientes/ambiente1/ambiente1.module';
+import { Ambiente2Module } from './ambientes/ambiente2/ambiente2.module';
+import { Ambiente3Module } from './ambientes/ambiente3/ambiente3.module';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    AmbientesComponent,
+    NavbarComponent,
+    FooterComponent,
+    FormularioComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    Ambiente1Module,
+    Ambiente2Module,
+    Ambiente3Module
+  ],
+  exports: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    AmbientesComponent,
+    NavbarComponent,
+    FooterComponent,
+    RouterModule,
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
